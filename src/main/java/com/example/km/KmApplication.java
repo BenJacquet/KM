88bf6c1f0d7549471da88cf7349376abbf35a4f2
benjacquet <jabenjam@student.42.fr>
2023-06-07 11:41:46 +0200

    first commit

---
+++ src/main/java/com/example/km/KmApplication.java
@@ -0,0 +1,13 @@
+package com.example.km;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KmApplication.class, args);
+	}
+
+}
